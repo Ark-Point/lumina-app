db830cba1c9f90cd5e58bcf4e71322e6a3f92503
ghks18g <ghks18g@gmail.com>
2025-10-24 11:03:24 +0900

    feat(apps/agent): remove coin-metadata

--- apps/agent/src/storage/storage.module.ts
+++ apps/agent/src/storage/storage.module.ts
@@ -2,16 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CoinDeployment } from "./coin/coin-deployment.entity";
 import { CoinDeploymentRepository } from "./coin/coin-deployment.repo";
-import { CoinMetadata } from "./coin/coin-metadata.entity";
-import { CoinMetadataRepository } from "./coin/coin-metadata.repo";
 import { MessageEntity } from "./message/message.entity";
 import { MessageRepo } from "./message/message.repo";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([MessageEntity, CoinMetadata, CoinDeployment]),
-  ],
-  providers: [MessageRepo, CoinMetadataRepository, CoinDeploymentRepository],
-  exports: [MessageRepo, CoinMetadataRepository, CoinDeploymentRepository],
+  imports: [TypeOrmModule.forFeature([MessageEntity, CoinDeployment])],
+  providers: [MessageRepo, CoinDeploymentRepository],
+  exports: [MessageRepo, CoinDeploymentRepository],
 })
 export class StorageModule {}
